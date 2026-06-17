5071ca419ee586592911aa0483740c3806d1e030
MohdMufeet <mohdmufeet786@gmail.com>
2026-06-17 12:01:10 +0530

    fix: correct loading state management in AdminLoginFom component

--- src/components/Auth/Admin/AdminLoginFom.jsx
+++ src/components/Auth/Admin/AdminLoginFom.jsx
@@ -6,7 +6,7 @@ import Wrapper from "../../Common/Wrapper";
 import { useNavigate } from "react-router-dom";
 
 const AdminLoginFom = () => {
-
+  const [loading, setLoaing] = React.useState(false);
   const [serverData, setServerData] = React.useState(null);
   const [formData, setFormData] = React.useState({
     email: "",
@@ -27,13 +27,13 @@ const AdminLoginFom = () => {
   const handleSubmit = async (e) => {
 
     e.preventDefault();
-
+    setLoading(true);
     const admin = await adminLogin(formData);
 
     setServerData(admin);
 
     if (admin.success && admin.data.token) {
-      localStorage.setItem("token", admin.data.token);
+      // localStorage.setItem("token", admin.data.token);
       await loginAdminGlobal(admin.data);
 
       setFormData({
@@ -41,6 +41,7 @@ const AdminLoginFom = () => {
         password: "",
       });
     }
+    setLoaing(false);
   };
 
 
